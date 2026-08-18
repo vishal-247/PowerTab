a621e50b148503a8bc2690346a56351aa9d42d3c
VISHAL VISHWAKARMA <135523652+vishal-247@users.noreply.github.com>
2026-08-18 20:02:48 +0530

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App(){
+  return (
+    <div className="app">
+      <div className="canvas">
+        <h1>My Infinite Canvas</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App;
